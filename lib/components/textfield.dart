dab645768814c545bd60571d824843428b2b66ed
GIODIROSA <giodirosaartes@gmail.com>
2025-09-11 23:08:30 -0300

    [ADD] build textfield

--- lib/components/textfield.dart
+++ lib/components/textfield.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class TextFieldExample extends StatelessWidget {
@@ -6,6 +5,59 @@ class TextFieldExample extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return ListView(
+      children: [
+        SizedBox(height: 60),
+        TextField(),
+        SizedBox(height: 32),
+        TextField(),
+        SizedBox(height: 32),
+        TextField(decoration: InputDecoration(hintText: "Introduce tu email")),
+        SizedBox(height: 32),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Introduce tu email",
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "Introduce tu email",
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "Introduce tu contraseña",
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: TextField(
+            maxLines: 1,
+            maxLength: 10,
+            decoration: InputDecoration(
+              hintText: "Introduce tu comentario",
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+        SizedBox(height: 32),
+      ],
+    );
   }
 }
